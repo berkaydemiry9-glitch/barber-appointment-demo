875afa1f4bf87012ccfd86574765a38405a86a0f
berkaydemiry9-glitch <berkaydemiry9@gmail.com>
2025-08-15 11:50:50 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Atlanta Barber Reservation System</h1>
+      <p>Welcome! Book your haircut appointment now.</p>
+    </div>
+  );
+}
+
+export default App;
